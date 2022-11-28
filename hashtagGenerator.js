3bf9f656ae9f4776a9c943efb1faedf2e3d624f4
Lyvnatt <johanaec.2017@gmail.com>
2022-11-28 16:53:23 -0500

    first commit

---
+++ hashtagGenerator.js
@@ -0,0 +1,13 @@
+function hashtagGenerator (str) {
+  str = str.split(' ').map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join('');
+
+  str = '#' + str;
+
+  return str.length > 140 || str.length == 1 ? false : str;
+}
+
+hashtagGenerator(" Hello there thanks for trying my Kata");
+hashtagGenerator("    Hello     World   ");
+hashtagGenerator("");
